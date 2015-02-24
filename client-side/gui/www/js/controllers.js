9240696fd345eabd00574a2685bdd2b7c4c97c83
Shavit Cohen <shavit@tikalk.com>
2015-02-24 21:09:10 +0200

    remarking map initiation

--- client-side/gui/www/js/controllers.js
+++ client-side/gui/www/js/controllers.js
@@ -67,17 +67,14 @@ angular.module('starter.controllers', [])
         var zoom = 3;
 
 
-        var lonLat = new OpenLayers.LonLat( -0.1279688 ,51.5077286 )
+        /*var lonLat = new OpenLayers.LonLat( -0.1279688 ,51.5077286 )
             .transform(
             new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
             map.getProjectionObject() // to Spherical Mercator Projection
         );
 
         markers.addMarker(new OpenLayers.Marker(lonLat));
-        map.setCenter(lonLat, zoom);
-
-
-
+        map.setCenter(lonLat, zoom);*/
         $scope.showCheckins = function(){
 
             $http({
